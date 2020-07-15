162ee1e731adc75869f7ab0dc00b713bf6d4c69b
Matt Mapplebeck <mmapplebeck@flatiron.com>
2020-07-15 13:42:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Chart from "react-google-charts";
+
+import {getPaths, getResults} from './chart.service'
 
 function App() {
+  const [data, setData] = useState(getPaths('Demographics'))
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Search
+      <input type="text" value={query} onChange={e => {
+        setQuery(e.target.value)
+        setResults(getResults(e.target.value))
+      }} />
+      {results.length > 0 && (
+        <div>
+          Results
+          {results.map(result => (
+            <div>
+              <button key={result} onClick={() => {
+                setData(getPaths(result))
+                setResults([])
+                setQuery('')
+              }}>{result}</button>
+            </div>
+          ))}
+        </div>
+      )}
+      
+      <Chart
+        width={600}
+        height={'300px'}
+        chartType="Sankey"
+        loader={<div>Loading Chart</div>}
+        data={data}
+        options={{
+          sankey: {
+            node: {
+              interactivity: true
+            }
+          }
+        }}
+        chartEvents={[
+          {
+            eventName: 'select',
+            callback: ({ chartWrapper }) => {
+              const chart = chartWrapper.getChart()
+              const selection = chart.getSelection()
+              if (selection.length === 1) {
+                const [selectedItem] = selection
+                const {name} = selectedItem
+                setData(getPaths(name))
+              }
+            },
+          }
+        ]}
+      />
     </div>
   );
 }
