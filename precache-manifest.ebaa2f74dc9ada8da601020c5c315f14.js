d04fd25a18dd560d3a2c7595c5175abd39702f49
Matt Mapplebeck <mmapplebeck@flatiron.com>
2020-07-15 13:54:08 -0400

    Updates

---
+++ precache-manifest.ebaa2f74dc9ada8da601020c5c315f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f482858be9e4326b067a4258255f9ee",
+    "url": "/paths-to-glory/index.html"
+  },
+  {
+    "revision": "2765fa630ad0dc72fb81",
+    "url": "/paths-to-glory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fca06c90ebc3ed449529",
+    "url": "/paths-to-glory/static/js/2.79bbd531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paths-to-glory/static/js/2.79bbd531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2765fa630ad0dc72fb81",
+    "url": "/paths-to-glory/static/js/main.45a1f76e.chunk.js"
+  },
+  {
+    "revision": "3fde74d9e272454d9ed9",
+    "url": "/paths-to-glory/static/js/runtime-main.41c0f666.js"
+  }
+]);
