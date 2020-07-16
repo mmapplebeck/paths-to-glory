904b743f0db85ea51bd7232a0dc3b3f84cf0a76a
Matt Mapplebeck <mmapplebeck@flatiron.com>
2020-07-16 14:36:08 -0400

    use https

--- src/app.service.js
+++ src/app.service.js
@@ -1,6 +1,6 @@
 import superagent from 'superagent'
 
-const baseUrl = 'http://10.110.101.254/nodes'
+const baseUrl = 'https://10.110.101.254/nodes'
 
 const cycleSuffix = ' (cycle)'
 
@@ -29,10 +29,10 @@ class AppService {
     ]
     const node = paths.node
     const usedNames = new Set(paths.previous.nodes.map(p => p.name))
-    const previous = paths.previous.nodes.filter(p => p.name !== node.name).sort((a, b) => b.count - a.count).slice(0, 10).map(p => (
+    const previous = paths.previous.nodes.filter(p => p.name !== node.name).sort((a, b) => b.count - a.count).map(p => (
       [p.name, node.name, p.count]
     ))
-    const next = paths.next.nodes.filter(n => n.name !== node.name).sort((a, b) => b.count - a.count).slice(0, 10).map(n => (
+    const next = paths.next.nodes.filter(n => n.name !== node.name).sort((a, b) => b.count - a.count).map(n => (
       [node.name, usedNames.has(n.name) ? addCycleSuffix(n.name) : n.name, n.count]
     ))
     return [...transformed, ...previous, ...next]
